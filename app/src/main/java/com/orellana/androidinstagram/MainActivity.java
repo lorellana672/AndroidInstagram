46a295431e33f7f56270b129028db22b7c0921c7
lorellana672 <lorellana672@famaf.unc.edu.ar>
2020-03-13 19:01:47 -0300

    login - Creating the auth for fire base.

--- app/src/main/java/com/orellana/androidinstagram/MainActivity.java
+++ app/src/main/java/com/orellana/androidinstagram/MainActivity.java
@@ -1,5 +1,6 @@
 package com.orellana.androidinstagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,13 +10,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthRegistrar;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText password;
-    EditText userText;
+    String password;
+    String userText;
     Boolean signUpMode = true;
     TextView buttonChangeText;
     Button signUpButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     public void onClick(View view) {
@@ -37,17 +46,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void signUp(View view) {
 
-        userText = findViewById(R.id.userText);
-        password = findViewById(R.id.password);
+        userText = findViewById(R.id.userText).toString().trim();
+        password = findViewById(R.id.password).toString().trim();
 
-        if (userText.getText().toString().equals("") || password.getText().toString().equals("")){
+        if (userText.equals("") || password.equals("")){
             Toast.makeText(this, "User and Password are required", Toast.LENGTH_SHORT).show();
-        } else if (userText.getText().length() <= 3){
+        } else if (userText.length() <= 3){
             Toast.makeText(this, "The user name is to short", Toast.LENGTH_SHORT).show();
-        } else if (password.getText().length() <= 4){
+        } else if (password.length() <= 4){
             Toast.makeText(this, "The password is to short", Toast.LENGTH_SHORT).show();
         }
+
+        mAuth.createUserWithEmailAndPassword(userText, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "User registered", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error. User not registered", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,5 +79,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonChangeText = findViewById(R.id.buttonChangeText);
         buttonChangeText.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
 }
