915538757b1686a62b8e55b2a3f00131bc3b9ecf
lorellana672 <lorellana672@famaf.unc.edu.ar>
2020-03-16 17:44:02 -0300

    login - Users can now register.

--- app/src/main/java/com/orellana/androidinstagram/MainActivity.java
+++ app/src/main/java/com/orellana/androidinstagram/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,14 +18,31 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    String password;
-    String userText;
+    EditText password;
+    EditText userText;
     Boolean signUpMode = true;
     TextView buttonChangeText;
     Button signUpButton;
     ProgressDialog progressDialog;
 
     private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        userText = findViewById(R.id.userText);
+        password = findViewById(R.id.password);
+
+        signUpButton = findViewById(R.id.signUpButton);
+        buttonChangeText = findViewById(R.id.buttonChangeText);
+        buttonChangeText.setOnClickListener(this);
+        signUpButton.setText("Signup");
+
+        progressDialog = new ProgressDialog(this);
+    }
 
     @Override
     public void onClick(View view) {
@@ -44,19 +63,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void signUp(View view) {
 
-        userText = findViewById(R.id.userText).toString().trim();
-        password = findViewById(R.id.password).toString().trim();
+        String user = userText.getText().toString().trim();
+        String pass = password.getText().toString().trim();
 
-        if (userText.equals("") || password.equals("")){
+        if (user.equals("") || pass.equals("")){
             Toast.makeText(this, "User and Password are required", Toast.LENGTH_SHORT).show();
-        } else if (userText.length() <= 3){
+        } else if (user.length() <= 3){
             Toast.makeText(this, "The user name is to short", Toast.LENGTH_SHORT).show();
-        } else if (password.length() <= 4){
+        } else if (pass.length() <= 4){
             Toast.makeText(this, "The password is to short", Toast.LENGTH_SHORT).show();
         } else {
             progressDialog.setMessage("Loading");
             progressDialog.show();
-            createAcount(userText, password);
+            createAcount(user, pass);
         }
     }
 
@@ -74,22 +93,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 });
     }
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-
-        signUpButton = findViewById(R.id.signUpButton);
-        buttonChangeText = findViewById(R.id.buttonChangeText);
-        buttonChangeText.setOnClickListener(this);
-        signUpButton.setText("Signup");
-
-        progressDialog = new ProgressDialog(this);
-    }
-
 
 }
